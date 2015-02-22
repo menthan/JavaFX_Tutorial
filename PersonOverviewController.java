3426fdfa54b8d1d5a8446fa76e2df601728e3ba1
Frederic Lott <ulfl234@gmail.com>
2015-02-22 19:04:54 +0100

    Create PersonOverviewController.java
    as of tutorial step 2

---
+++ PersonOverviewController.java
@@ -0,0 +1,58 @@
+package ch.makery.address.view;
+
+import ch.makery.address.MainApp;
+import ch.makery.address.model.Person;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
+public class PersonOverviewController {
+	@FXML
+	private TableView<Person> personTable;
+	@FXML
+	private TableColumn<Person, String> firstNameColumn;
+	@FXML
+	private TableColumn<Person, String> lastNameColumn;
+
+	@FXML
+	private Label firstNameLabel;
+	@FXML
+	private Label lastNameLabel;
+	@FXML
+	private Label streetLabel;
+	@FXML
+	private Label postalCodeLabel;
+	@FXML
+	private Label cityLabel;
+	@FXML
+	private Label birthdayLabel;
+
+	// reference to the main application
+	private MainApp mainApp;
+
+	public PersonOverviewController() {
+	}
+
+	/**
+	 * Initializes the controller class. This method is automatically called
+	 * after the fxml file has been loaded.
+	 */
+	@FXML
+	private void initialize() {
+		// initialize the person table with the two columns.
+		firstNameColumn.setCellValueFactory(cellData -> cellData.getValue().firstNameProperty());
+		lastNameColumn.setCellValueFactory(cellData -> cellData.getValue().lastNameProperty());
+	}
+	
+	/**
+	 * Is called by the main application to give a reference back to itself.
+	 * @param mainApp
+	 */
+	public void setMainApp(MainApp mainApp){
+		this.mainApp = mainApp;
+		
+		//Add observable list data to the table
+		personTable.setItems(mainApp.getPersonData());
+	}
+}
